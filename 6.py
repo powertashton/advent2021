3eb1cb943fc630adde8c7a6ec23eeb414f01b628
Ashton Power <48048360+powertashton@users.noreply.github.com>
2021-12-06 19:53:18 +0800

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,16 +1,100 @@
 array = open('6.txt', "r").read().split(',')
 print(array)
 day = 0
+one = array.count('1')
+two = array.count('2')
+three = array.count('3')
+four = array.count('4')
+five = array.count('5')
+six = array.count('6')
+seven = array.count('7')
+eight = array.count('8')
 
-while day < 256 :
+
+# ones = [1]
+# count = 0
+# day = 0
+# while day < 80 :
+# 	#print(day, ',',len(ones))
+# 	i = 0
+# 	#print('1:',day)
+# 	for x in ones : 
+# 		ones[i] = int(ones[i]) - 1
+# 		if int(ones[i]) == -1 :
+# 			ones[i] = 6
+# 			ones.append('9')
+# 		i += 1
+# 	day += 1
+# 	
+# twos = [2]
+# count = 0
+# day = 0
+# while day < 80 :
+# 	#print(day, ',',len(twos))
+# 	i = 0
+# 	#print('2:',day)
+# 	for x in twos : 
+# 		twos[i] = int(twos[i]) - 1
+# 		if int(twos[i]) == -1 :
+# 			twos[i] = 6
+# 			twos.append('9')
+# 		i += 1
+# 	day += 1
+	
+threes = [3]
+count = 0
+day = 0
+while day < 80 :
+	#print(day, ',',len(threes))
 	i = 0
-	for x in array : 
-		array[i] = int(array[i]) - 1
-		if int(array[i]) == -1 :
-			array[i] = 6
-			array.append('9')
+	#print('3:',day)
+	for x in threes : 
+		threes[i] = int(threes[i]) - 1
+		if int(threes[i]) == -1 :
+			threes[i] = 6
+			threes.append('9')
 		i += 1
 	day += 1
 
-print(len(array))
+# fours = [4]
+# count = 0
+# day = 0
+# while day < 80 :
+# 	#print(day, ',',len(fours))
+# 	i = 0
+# 	#print('4:',day)
+# 	for x in fours : 
+# 		fours[i] = int(fours[i]) - 1
+# 		if int(fours[i]) == -1 :
+# 			fours[i] = 6
+# 			fours.append('9')
+# 		i += 1
+# 	day += 1
 
+# fives = [5]
+# count = 0
+# day = 0
+# while day < 80 :
+# 	print(day, ',',len(fives))
+# 	i = 0
+# 	#print('5:',day)
+# 	for x in fives : 
+# 		fives[i] = int(fives[i]) - 1
+# 		if int(fives[i]) == -1 :
+# 			fives[i] = 6
+# 			fives.append('9')
+# 		i += 1
+# 	day += 1
+
+
+	
+	
+
+
+print(len(threes)) #37
+
+
+print(len(threes)*one+len(threes)*two+len(threes)*three+len(threes)*four+len(threes)*five)
+
+#346200 is too low
+#5550000000000 too high
